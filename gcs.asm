84db97c429fcc109d0f697878006a43b5327b7f9
Gregory Norton <gregory.norton@me.com>
2019-02-17 19:10:04 -0500

    Added stack initialization and changed fixed data instruction

--- gcs.asm
+++ gcs.asm
@@ -6,6 +6,8 @@ BANKEDRAM:  .EQU    0x4000
 IO:         .EQU    0x8000
 ROM:        .EQu    0xC000
 
+SYSSTACK:   .EQU    0x3FFF
+
 VECTORADDR: .EQU    0xFFF0
 NMI:        .EQU    0x0000
 SWI:        .EQU    0x0000
@@ -16,14 +18,15 @@ SWI3:       .EQU    0x0000
 
 
     ORG ROM
-
+    LDS #SYSSTACK   ; Initialize system stack
+    
 
     ORG VECTORADDR
 vectors:
-.reserved:  .FDB    0
-.swi3:      .FDB    SWI3
-.swi2:      .FDB    SWI2
-.irq:       .FDB    IRQ
-.swi:       .FDB    SWI
-.nmi:       .FDB    NMI
-.reset:     .FDB    ROM
+.reserved:  .DB     0
+.swi3:      .DB     SWI3
+.swi2:      .DB     SWI2
+.irq:       .DB     IRQ
+.swi:       .DB     SWI
+.nmi:       .DB     NMI
+.reset:     .DB     ROM
